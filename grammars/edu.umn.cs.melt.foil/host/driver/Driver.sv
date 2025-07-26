a11805fc4e9513ec692437bb56e11989a84af58b
Lucas Kramer <krame505@umn.edu>
2025-07-26 12:55:09 -0500

    Check core and l1 errors in driver

--- grammars/edu.umn.cs.melt.foil/host/driver/Driver.sv
+++ grammars/edu.umn.cs.melt.foil/host/driver/Driver.sv
@@ -3,6 +3,7 @@ grammar edu:umn:cs:melt:foil:host:driver;
 imports edu:umn:cs:melt:foil:host:concretesyntax as cnc;
 imports edu:umn:cs:melt:foil:host:langs:ext as ext;
 imports edu:umn:cs:melt:foil:host:langs:core as core;
+imports edu:umn:cs:melt:foil:host:langs:l1 as l1;
 
 imports silver:langutil;
 imports silver:langutil:pp;
@@ -10,8 +11,20 @@ imports silver:langutil:pp;
 fun driver IO<Integer> ::= args::[String] parse::(ParseResult<cnc:Root> ::= String String) = do {
   extAst :: Decorated ext:Root <- parseDriver(args, parse);
   when_(!null(extAst.errors), do {
-    print(messagesToString(extAst.errors) ++ "\n");
+    println(messagesToString(extAst.errors));
     exit(3);
+  });
+  let coreAst :: Decorated core:Root = extAst.ext:toCore;
+  when_(!null(coreAst.errors), do {
+    println("Errors in core AST!");
+    println(messagesToString(coreAst.errors));
+    exit(4);
+  });
+  let l1Ast :: Decorated l1:Root = coreAst.l1:toL1;
+  when_(!null(l1Ast.errors), do {
+    println("Errors in L1 AST!");
+    println(messagesToString(l1Ast.errors));
+    exit(5);
   });
   return 0;
 };
@@ -23,7 +36,7 @@ fun parseDriver IO<Decorated ext:Root> ::= args::[String] parse::(ParseResult<cn
   text <- readFile(fileName);
   let parseResult = parse(text, fileName);
   when_(!parseResult.parseSuccess, do {
-    print(parseResult.parseErrors ++ "\n");
+    println(parseResult.parseErrors);
     exit(2);
   });
   let cst = decorate parseResult.parseTree with {};
